c4f929394ebfd539f2aa0600f104e8650487595c
Студент <ab2@EU.NET>
2018-05-31 10:34:40 +0300

    Add words statistics

--- main.js
+++ main.js
@@ -1,16 +1,44 @@
 let fs = require('fs');
-fs.readFileSync('spam.csv')
+
+let wordsData = {ham: {}, spam: {}};
+let totalAmount = null;
+let totalSpamLines = 0;
+let totalHamLines = 0;
+
+let lines = fs.readFileSync('spam.csv')
     .toString()
     .split(/\r?\n/)
     .forEach(line => {
         let category = line.split(',')[0];
+        if (category === 'spam') totalSpamLines++;
+        else if (category === 'ham') totalHamLines++;
+
         let text = line.substring(category.length + 1).toLowerCase();
         let words = text
             .replace(/[^A-Z' ]+/ig, ' ')
             .trim()
-            .split(/\s+/);
-        
-        console.log(words);    
+            .split(/\s+/)
+            .reduce((finalDataObj, word) => {
+                if (category === 'ham') {
+                    if (Object.is(finalDataObj.ham[word], undefined)) {
+                       finalDataObj.ham[word] = 0;
+                       return finalDataObj;
+                    }
+                    finalDataObj.ham[word] += 1;
+                    return finalDataObj;
+                }
+               
+                if (category === 'spam') {
+                    if (Object.is(finalDataObj.spam[word], undefined)) {
+                        finalDataObj.spam[word] = 0;
+                        return finalDataObj;    
+                    }
+                    finalDataObj.spam[word] += 1;
+                    return finalDataObj; 
+                }
+                
+            }, wordsData);   
     });
+    totalAmount = Object.getOwnPropertyNames(wordsData.ham).length + Object.getOwnPropertyNames(wordsData.spam).length;
 
 //callsminmoremobsemspoboxpowa
